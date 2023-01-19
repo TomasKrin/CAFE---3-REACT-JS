8daa516266c9aec3fe77c19a6c2ec159850af9ee
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-01-19 19:04:53 +0200

    formatter set up

--- 8_paskaita/src/App.js
+++ 8_paskaita/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import Button from './Components/Button/Button';
-import styled from 'styled-components';
-import CustomButton from './Components/CustomButton.js/CustomButton';
-import CustomInput from './Components/CustomInput/CustomInput';
+import "./App.css";
+import Button from "./Components/Button/Button";
+import styled from "styled-components";
+import CustomButton from "./Components/CustomButton.js/CustomButton";
+import CustomInput from "./Components/CustomInput/CustomInput";
 
 const Container = styled.div`
   padding: 40px;
@@ -15,24 +15,28 @@ const TomatoButton = styled(CustomButton)`
 `;
 
 //size: 'sm' | 'lg' | 'xl'
-const props = {size: 'xl', inputColor: 'rebeccapurple'};
-
+const props = { size: "xl", inputColor: "rebeccapurple" };
 
 function App() {
   return (
     <div>
       <Container>
-        <Button/>
-      </Container>  
-      <Container>
-        <CustomButton>Mano Button</CustomButton>
-        <TomatoButton as='a' href='https://www.google.com/'>Mano Pomidorinis Mygtukas</TomatoButton>
+        <Button />
       </Container>
       <Container>
-        <CustomInput defaultValue='Tomas'/>
-        <CustomInput defaultValue='Tomas' inputColor='red' size='lg'/>
-        <CustomInput defaultValue='Tomas' inputColor={props.inputColor}
-        size={props.size}/>
+        <CustomButton>Mano Button</CustomButton>
+        <TomatoButton as="a" href="https://www.google.com/">
+          Mano Pomidorinis Mygtukas
+        </TomatoButton>
+      </Container>
+      <Container>
+        <CustomInput defaultValue="Tomas" />
+        <CustomInput defaultValue="Tomas" inputColor="red" size="lg" />
+        <CustomInput
+          defaultValue="Tomas"
+          inputColor={props.inputColor}
+          size={props.size}
+        />
       </Container>
     </div>
   );
