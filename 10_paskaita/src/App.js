cf25c67ede8b2002fc2308742cded6dd604cbde4
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-01-25 22:40:11 +0200

    rename

--- 10_paskaita/src/App.js
+++ 10_paskaita/src/App.js
@@ -1,15 +1,5 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-
-function App() {
-  return (
-    <Routes>
-      <Route path={"/"} element={<MainPage />} />
-      <Route path={""} element={""} />
-      <Route path={""} element={""} />
-    </Routes>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
