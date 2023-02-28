8fbf84a7b138d2eab86bad8bc7794c91470e63a9
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-28 17:47:29 +0200

    react-modal setup

--- job_board/src/index.tsx
+++ job_board/src/index.tsx
@@ -5,10 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import Modal from "react-modal";
 
 const queryClient = new QueryClient();
+Modal.setAppElement("#root");
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
