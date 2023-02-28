02f52e6aa0ac7a172aee780caadb69e4ac81496d
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-28 17:47:55 +0200

    come api type corrections

--- job_board/src/api/users.ts
+++ job_board/src/api/users.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { User } from "../types/apis";
+import { User, LoginUser, NewUser } from "../types/user";
 
 const USERS_API_URL = "https://testapi.io/api/rokasandreikenas/resource/user";
 
@@ -14,18 +14,20 @@ export const fetchUsers = (): Promise<User[]> => {
 };
 
 // Aprasyti kurimui skirta tipa Omit<User, laukas>
-type CreateUserRequest = Omit<User, "id" | "createdAt" | "updatedAt">;
 
-export const createUser = (user: CreateUserRequest): Promise<User> => {
+export const createUser = (user: NewUser): Promise<User> => {
   return axios.post(USERS_API_URL, user).then((response) => response.data);
 };
 
-export const loginUser = async (loggingUser: User): Promise<User> => {
+export const loginUser = async (loggingUser: LoginUser): Promise<User> => {
   const users = await fetchUsers();
   return new Promise((resolve, reject) => {
     const { email, password } = loggingUser;
+
     const userChecker = (u: User) => u.email === email && u.password === password;
+
     const existingUser = users.find(userChecker);
+
     existingUser ? resolve(existingUser) : reject("Invalid credentials");
   });
 };
